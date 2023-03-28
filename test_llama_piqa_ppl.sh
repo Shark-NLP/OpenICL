a257c929ecd670d5fb5597da3105bff7497a4d49
numbmelon <1157392430@qq.com>
2023-03-28 12:54:01 +0800

    temporarily remove test files.

--- test_llama_piqa_ppl.sh
+++
@@ -1,6 +0,0 @@
-export http_proxy=http://172.16.1.135:3128/
-export https_proxy=http://172.16.1.135:3128/
-export HTTP_PROXY=http://172.16.1.135:3128/
-export HTTPS_PROXY=http://172.16.1.135:3128/
-
-srun -p moss --gres=gpu:1 --quotatype=spot --async -o ./output/llama_ppl_piqa.log -J llama_ppl_piqa python test_llama_piqa_ppl.py
