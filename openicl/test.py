a7e815c1540ea27fb7ee492501fc49d5274c19bf
cookie1077 <cookie1077@naver.com>
2023-10-03 22:19:27 +0900

    tried sst5

--- openicl/test.py
+++ openicl/test.py
@@ -2,18 +2,34 @@
 # added more codes in the main branch. 
 
 from datasets import load_dataset
+from datasets import Dataset
 from openicl import DatasetReader
 
+'''
+def gen(shards):
+    for shard in shards:
+        with open(shard) as f:
+            for line in f:
+                yield {"line": line}
+shards = [f"data{i}.txt" for i in range(32)]
+ds = Dataset.from_generator(gen, gen_kwargs={"shards": shards})
+'''
+
+
 # Loading dataset from huggingface
-dataset = load_dataset('gpt3mix/sst2')
+# lets do sst5
+dataset = load_dataset('SetFit/sst5')
 
 # Define a DatasetReader, with specified column names where input and output are stored.
 data = DatasetReader(dataset, input_columns=['text'], output_column='label')
 
 from openicl import PromptTemplate
 tp_dict = {
-    0: "</E>Positive Movie Review: </text>",
-    1: "</E>Negative Movie Review: </text>" 
+    0: "</E>Very Negative Movie Review: </text>",
+    1: "</E>Negative Movie Review: </text>",
+    2: "</E>Neutral Movie Review: </text>" ,
+    3: "</E>Positive Movie Review: </text>" ,
+    4: "</E>Very Positive Movie Review: </text>" 
 }
 
 template = PromptTemplate(tp_dict, {'text': '</text>'}, ice_token='</E>')
